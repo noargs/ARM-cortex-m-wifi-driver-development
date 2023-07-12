cec939cff046979c08427c354734f98d01fc5b88
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-13 00:47:38 +0100

    systick timer project init

---
+++ 04_timebase/Inc/esp82xx_driver.h
@@ -0,0 +1,14 @@
+#ifndef ESP82XX_DRIVER_H_
+#define ESP82XX_DRIVER_H_
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+#define SR_TXE                    (1U << 7)
+#define SR_RXNE                   (1U << 5)
+
+void debug_uart_init(void);
+void debug_uart_write(int ch);
+void esp_uart_init(void);
+
+#endif /* ESP82XX_DRIVER_H_ */
