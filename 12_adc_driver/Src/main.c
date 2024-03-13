549545c7595bcb3a9ceafacee86b4ba21afee462
Ibn Asghar <asim.ibn.asghar@gmail.com>
2024-03-13 16:58:45 +0000

    sensor to cloud project init, adc driver proj clean up

--- 12_adc_driver/Src/main.c
+++ 12_adc_driver/Src/main.c
@@ -1,45 +1,18 @@
-#include <stdio.h>
-#include "stm32f4xx.h"
-#include "esp82xx_driver.h"
-#include "circular_buffer.h"
-#include "esp82xx_lib.h"
-#include "hardware_modules.h"
 #include "adc.h"
-
-#define SSID_NAME                 "__hw_wifi__"
-#define PASSKEY                   "dude@dude.com"
 
 uint32_t sensor_value;
 
 int main()
 {
-//	debug_uart_init();
-
-//	esp_uart_init();
-
-	// RS pin is connected to PA8 instead of 3.3v
-//	esp_rs_pin_init();
-//	esp_rs_pin_enable();
-
-	// Initialise hardware modules
-//	light_init();
-//	valve_init();
-//	pump_init();
-//	gripper_init();
-
 	// Initialise ADC
 	pa1_adc_init();
 
 	// start conversion ADC
 	start_conversion();
 
-//	esp82xx_server_init(SSID_NAME, PASSKEY);
 
 	while(1)
 	{
-		// Activate the server
-//		esp82xx_server_begin();
-
 		sensor_value = adc_read();
 	}
 }
